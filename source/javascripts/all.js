5f722d541ba6d1bd59e19b38aef2ddb87471f0c4
Salim Batlouni <batlounis@gmail.com>
2012-12-16 13:17:42 +0000

    pictures added + sponsors functionality

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,13 @@
 //= require_tree .
+
+toggleSponsors = function(){
+	var sponsors = document.getElementById('sponsors');
+	
+	if(sponsors.className == 'show'){
+		sponsors.className = '';
+	}else{
+		sponsors.className = 'show';
+	}
+	
+	return false;
+}
